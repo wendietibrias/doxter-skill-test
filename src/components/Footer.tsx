c59286b43b3311621f58d9d52209ccba98789527
wendietibrias <wendietibrias@gmail.com>
2024-03-23 23:51:11 +0700

    fix: footer layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,22 +8,22 @@ const Footer = () => {
                     <a href="javascript:void(0)" id="logo">Talentive</a>
                     <p>At Talentive, we believe in the power of creativity to transform ideas into impactful experiences.</p>
                 </div>
+                <div className="footer--content-item-3">
+                    <h4>Navigations</h4>
+                    <ul>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">About Us</a></li>
+                        <li><a href="#service">Services</a></li>
+                        <li><a href="#portofolio">Portofolio</a></li>
+                        <li><a href="#contact">Contact Us</a></li>
+
+                    </ul>
+                </div>
                 <div className="footer--content-item-2">
                     <h4>Contact Info</h4>
                     <p>Jalan Imam Bonjol,Gang Alakadabarnya,No 1 dan 2 samping Alfamart,Pontianak Indonesia</p>
                     <br/>
                     <p>Number phone : 0895-3875-1451</p>
-                </div>
-                <div className="footer--content-item-3">
-                    <h4>Navigations</h4>
-                    <ul>
-                        <li><a>Home</a></li>
-                        <li><a>About Us</a></li>
-                        <li><a>Services</a></li>
-                        <li><a>Portofolio</a></li>
-                        <li><a>Contact Us</a></li>
-
-                    </ul>
                 </div>
             </div>
         </footer>
